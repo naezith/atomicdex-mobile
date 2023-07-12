557e39a0183ff889448ca84de6a51d9d2b366383
naezith <naezith@gmail.com>
2023-07-12 16:46:31 +0300

    implement VolumeBarGraphItem

---
+++ lib/packages/trading/widgets/payment_method/volume_bar_graph_item.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:komodo_dex/packages/trading/widgets/payment_method/bar_graph_item.dart';
+
+class VolumeBarGraphItem extends StatelessWidget {
+  final double value;
+  final Widget title;
+  final Widget valueLabel;
+  final VoidCallback onTap;
+  final double upperBound;
+  final Widget prefix;
+
+  const VolumeBarGraphItem({
+    @required this.value,
+    @required this.title,
+    @required this.upperBound,
+    @required this.prefix,
+    this.valueLabel,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BarGraphItem(
+      percentage: value,
+      percentageLabel: valueLabel,
+      tickerLabel: title,
+      upperBoundPercentage: upperBound,
+      lowerBoundPercentage: 0,
+      prefix: prefix,
+      positiveColor: Colors.blue,
+      hasVerticalLine: false,
+      valueIsPercentage: false,
+    );
+  }
+}
